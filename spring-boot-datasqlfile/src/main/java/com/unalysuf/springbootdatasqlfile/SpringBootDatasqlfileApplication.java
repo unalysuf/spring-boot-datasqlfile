b52f9d8354f37f5da5152ce8821e9622af691252
Yusuf Unal <unalysuf@gmail.com>
2023-03-13 17:41:23 +0300

    Firt Trial

---
+++ spring-boot-datasqlfile/src/main/java/com/unalysuf/springbootdatasqlfile/SpringBootDatasqlfileApplication.java
@@ -0,0 +1,13 @@
+package com.unalysuf.springbootdatasqlfile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDatasqlfileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDatasqlfileApplication.class, args);
+	}
+
+}
---
+++ spring-boot-datasqlfile/src/test/java/com/unalysuf/springbootdatasqlfile/SpringBootDatasqlfileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unalysuf.springbootdatasqlfile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDatasqlfileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
